a99db6da5f8bb53703b647f259ea44dc307e16cf
Yuri Lima <ylima@if.usp.br>
2026-02-04 12:08:17 -0300

    20260204

--- PWGJE/Tasks/jetSpectraCharged.cxx
+++ PWGJE/Tasks/jetSpectraCharged.cxx
@@ -512,7 +512,7 @@ struct JetSpectraCharged {
     }
   }
 
-  PROCESS_SWITCH(JetSpectraCharged, processD0JetsData, "jet spectra for Data with D0", false);
+  PROCESS_SWITCH(JetSpectraCharged, processD0JetsData, "jet spectra for Data with D0", true);
   // ¯·.¸¸.·´¯·.¸¸.·´¯·.¸¸.·´¯·.¸¸.·´¯·.¸¸.·´¯·.¸¸.·´¯·.¸¸.·´¯·.¸¸.·´><(((º>
 
   template <typename TJets>
